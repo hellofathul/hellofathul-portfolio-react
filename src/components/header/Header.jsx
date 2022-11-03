e4429718d15243be0d874c4c77529b7fa8796ce0
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-03 23:02:47 +0800

    Update Header Component CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,8 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Fathul Amin</h1>
-        <h5 className="text-light">Software Engineer, Graphic Designer</h5>
+        <h5 className="text-light">Web & Mobile Developer</h5>
+        <h5 className="text-light2">Graphic Designer</h5>
         <CTA />
         <HeaderSocials />
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {FaFacebookSquare} from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { FaFacebookSquare } from 'react-icons/fa'
+import './header.css'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/hellofathul/" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/hellofathul" target="_blank"><FaGithub /></a>
-        <a href="https://www.facebook.com/hellofathul/" target="_blank"><FaFacebookSquare /></a>
+      <a href="https://www.linkedin.com/in/hellofathul/" target="_blank"><BsLinkedin size={'2em'}/></a>
+      <a href="https://github.com/hellofathul" target="_blank"><FaGithub size={'2em'}/></a>
+      <a href="https://www.facebook.com/hellofathul/" target="_blank"><FaFacebookSquare size={'2em'}/></a>
     </div>
   )
 }
