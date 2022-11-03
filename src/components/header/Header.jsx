23e80c09cfc406be3f80348adccd41c6e2bbba89
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-03 14:49:48 +0800

    Installing React Icons and using it in the project
    -Icons for social media

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Fathul Amin</h1>
         <h5 className="text-light">Software Engineer, Graphic Designer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="ME" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FaFacebookSquare} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/hellofathul/" target="_blank"></a>
-        <a href="https://github.com/hellofathul" target="_blank"></a>
-        <a href="https://www.facebook.com/hellofathul/" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/hellofathul/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/hellofathul" target="_blank"><FaGithub /></a>
+        <a href="https://www.facebook.com/hellofathul/" target="_blank"><FaFacebookSquare /></a>
     </div>
   )
 }
