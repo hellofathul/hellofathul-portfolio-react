6a5b1a53e0d2b2bbdacdb5bba8e14e8216b4c007
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-04 00:11:14 +0800

    Floating Nav Component JSX

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsJournalCheck} from 'react-icons/bs'
+import {BsBriefcase} from 'react-icons/bs'
+import {TbMessageDots} from 'react-icons/tb'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsJournalCheck/></a>
+      <a href="#portfolio"><BsBriefcase/></a>
+      <a href="#contact"><TbMessageDots/></a>
+    </nav>
   )
 }
 
