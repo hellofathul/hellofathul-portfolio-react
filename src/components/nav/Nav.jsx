1a5dab95ed9636806ab347202518ac9c05e97ac5
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-04 11:52:26 +0800

    Managing Nav states using useState Hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BsJournalCheck} from 'react-icons/bs'
-import {BsBriefcase} from 'react-icons/bs'
-import {TbMessageDots} from 'react-icons/tb'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsJournalCheck } from 'react-icons/bs'
+import { BsBriefcase } from 'react-icons/bs'
+import { TbMessageDots } from 'react-icons/tb'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#" className='active'><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BsJournalCheck/></a>
-      <a href="#portfolio"><BsBriefcase/></a>
-      <a href="#contact"><TbMessageDots/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ' '}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ' '}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ' '}><BsJournalCheck /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ' '}><BsBriefcase /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ' '}><TbMessageDots /></a>
     </nav>
   )
 }
