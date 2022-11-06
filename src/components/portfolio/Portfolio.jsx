d8e38c2e769344295aa0b0da8a5b125afed9576e
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-07 00:42:46 +0800

    Generate portfolio from array using map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Student Induction Week (MINDS) Universiti Malaysia Pahang',
+    link: 'https://www.instagram.com/mindsump_2223/',
+    demo: 'https://dribbble.com/hellofathul',
+  }
+
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -9,16 +20,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>Student Induction Week (MINDS) Universiti Malaysia Pahang</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://dribbble.com/hellofathul" className='btn' target='_blank'>Dribbble</a>
-            <a href="https://www.instagram.com/mindsump_2223/" className='btn btn-primary' target='_blank'>MINDS Instagram</a>
-          </div>
-        </article>
+        {
+          data.map(({ id, image, title, link, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-img">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={link} className='btn' target='_blank'>Demo</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>MINDS Instagram</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
