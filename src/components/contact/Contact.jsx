a5d9725c2da68634839e6af0dbf370dda5dd47d4
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-07 14:41:09 +0800

    Form Submission using EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,26 @@ import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { RiMessengerLine } from 'react-icons/ri'
 import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gq4ldgv', 'template_3ka68vj', form.current, 'oAi_M01jbQoArVrBr')
+
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+      e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,9 +50,9 @@ const Contact = () => {
           </article>
         </div>
         {/* ========== END OF CONTACT ========*/}
-        <form action="">
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='to_name' placeholder='Your Full Name' required />
+          <input type="email" name='to_email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
